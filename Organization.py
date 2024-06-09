e310ee4890837c36302ee707134b2f134e8aa875
x-matvey-x <57106087+x-matvey-x@users.noreply.github.com>
2024-06-09 18:53:51 +0300

    сайтик

--- Organization.py
+++ Organization.py
@@ -1,75 +1,79 @@
 import pandas as pd
+from Test_api import get_tle_data
 
 def get_dataframe():
+    get_tle_data()
     sep = ' - '
+
     def org(string_part):
         str = string_part.strip(' ')
         if str == '':
             return None
         return str
+
     with open("TLE_data.txt", "r") as file:
         f = list(filter(None, file.read().replace("TBA - TO BE ASSIGNED", "TBA").split("\n")))
-        TLE_dict  = []
+        TLE_dict = []
         TBA_counter = 1
 
-    # https://www.space-track.org/documentation#/tle 
+        # https://www.space-track.org/documentation#/tle
 
         for index, i in enumerate(f, 1):
-            line_num =             org(i[0])
+            line_num = org(i[0])
             if line_num == '0':
-                name =             org(i[2:25])
+                name = org(i[2:25])
                 if name == 'TBA':
                     name = name + str(TBA_counter)
                     TBA_counter += 1
             if line_num == '1':
-                spu_num =          org(i[2:7])
-                сlassification =   org(i[7])
-                designator =       org(i[9:17])
-                epoch =            org(i[18:32])
-                derivative_1 =     org(i[33:43])
-                derivative_2 =     org(i[44:52])
-                braking_koef =     org(i[53:61])
+                spu_num = org(i[2:7])
+                сlassification = org(i[7])
+                designator = org(i[9:17])
+                epoch = org(i[18:32])
+                derivative_1 = org(i[33:43])
+                derivative_2 = org(i[44:52])
+                braking_koef = org(i[53:61])
                 element_set_type = org(i[62])
-                version_num =      org(i[64:68])
-                checksum_1 =       org(i[68])
+                version_num = org(i[64:68])
+                checksum_1 = org(i[68])
             if line_num == '2':
-                if spu_num !=      org(i[2:7]):
+                if spu_num != org(i[2:7]):
                     print("error")
-                orbit_degree =     org(i[8:16])
-                rAscofAscenNode =  org(i[17:25])
-                eccentricity =     org(i[26:33])
-                argPerigee =       org(i[34:42])
-                meanAnomaly =      org(i[43:51])
-                meanMotion =       org(i[52:63])
-                revolution_num =   org(i[63:68])
-                checksum_2 =       org(i[68])
+                orbit_degree = org(i[8:16])
+                rAscofAscenNode = org(i[17:25])
+                eccentricity = org(i[26:33])
+                argPerigee = org(i[34:42])
+                meanAnomaly = org(i[43:51])
+                meanMotion = org(i[52:63])
+                revolution_num = org(i[63:68])
+                checksum_2 = org(i[68])
 
                 TLE_dict.append({
-                        "Название": name,
-                        "Линия номер один": 1,
-                        "Номер спутника": spu_num,
-                        "Классификация": сlassification,
-                        "Международное обозначение": designator,
-                        "Эпоха": epoch,
-                        "Первая производная среднего движения": derivative_1,
-                        "Вторая производная среднего движения": derivative_2,
-                        "Коэффицент торможения": braking_koef,
-                        "Тип эфериды": element_set_type,
-                        "Номер элемента": version_num,
-                        "Контрольная сумма 1": int(checksum_1),
-                        "Линия номер 2": 2,
-                        "Наклонение в градусах": float(orbit_degree),
-                        "Долгота восходящего солнца": float(rAscofAscenNode),
-                        "Эксцентриситет": eccentricity,
-                        "Аргумент перицентра": float(argPerigee),
-                        "Средняя аномалия": float(meanAnomaly),
-                        "Средняя частота обращений": float(meanMotion),
-                        "Номер витка": int(revolution_num),
-                        "Контрольная сумма 2": int(checksum_2),
+                    "Название": name,
+                    "Линия номер один": 1,
+                    "Номер спутника": spu_num,
+                    "Классификация": сlassification,
+                    "Международное обозначение": designator,
+                    "Эпоха": epoch,
+                    "Первая производная среднего движения": derivative_1,
+                    "Вторая производная среднего движения": derivative_2,
+                    "Коэффицент торможения": braking_koef,
+                    "Тип эфериды": element_set_type,
+                    "Номер элемента": version_num,
+                    "Контрольная сумма 1": int(checksum_1),
+                    "Линия номер 2": 2,
+                    "Наклонение в градусах": float(orbit_degree),
+                    "Долгота восходящего солнца": float(rAscofAscenNode),
+                    "Эксцентриситет": eccentricity,
+                    "Аргумент перицентра": float(argPerigee),
+                    "Средняя аномалия": float(meanAnomaly),
+                    "Средняя частота обращений": float(meanMotion),
+                    "Номер витка": int(revolution_num),
+                    "Контрольная сумма 2": int(checksum_2),
                 })
-                    
-    df = pd.DataFrame.from_dict(TLE_dict)   
+
+    df = pd.DataFrame.from_dict(TLE_dict)
     sortDf_revolution = df.sort_values("Номер витка")
     sortDf_meanMotion = df.sort_values("Средняя частота обращений")
-    
+
     return df
--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for, render_template_string
-import pandas as pd
-import matplotlib.pyplot as plt
-import base64
+from flask import Flask, render_template, request, render_template_string, redirect, url_for, flash, session
 from Organization import get_dataframe
 from visualization.visualization import get_satellite_orb_img
 from visualization.utils import getSatellitesObjects
 import cv2
 
-app = Flask(__name__, static_url_path="", static_folder="public")
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.config['SECRET_KEY'] = 'wqefcb23y4br4hjgfj45'
 
 df = get_dataframe()
 satellites = getSatellitesObjects()
 
-@app.route('/')
-def index():
-    return df.to_html()
 
-@app.route('/satellite/<int:id>', methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    if request.method=="POST":
+        try:
+            if 0 <= int(request.form['sat_id']) < 21987:
+                return redirect(url_for('satellite', id = int(request.form['sat_id'])))
+            else:
+                flash("Неверные данные. Введите число от 0 до 21987.")
+        except ValueError:
+            flash("Неверные данные. Введите число от 0 до 21987.")
+    return render_template("main.html", title = "Трекинг спутников - Главная страница")
+
+
+
+
+@app.route("/df/")
+def df_info():
+    return render_template("dataframe_page.html", title = "TLE data", df = df)
+
+
+@app.route("/plots/")
+def plots():
+    return render_template("plot_info.html")
+
+
+@app.route('/satellite/<int:id>', methods=["GET", "POST"])
 def satellite(id):
     try:
         cv2.imwrite("public/satellite/img.png", get_satellite_orb_img(satellites[id]))
-        return render_template_string('<img src="img.png">')
+        return render_template('satellite.html', image = 'img.png', sat_name = satellites[id].satellite_name, id_next = "/satellite/"+str(min(id+1, 21986)), id_prev = "/satellite/"+str(max(id-1, 0)))
     except:
         return render_template_string(f"{satellites[id].satellite_name} was died")
 
@@ -28,3 +48,9 @@ if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
     app.run(debug=True)
+
+
+
+
+
+
